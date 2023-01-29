a65d9e25cf27d035a04ca7bd3a72293cc3138d68
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2023-01-29 22:25:28 +0530

    Create convert-bst-to-greater-tree.java

---
+++ 29-01-2023/convert-bst-to-greater-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    int sum = 0;
+    void greaterRoot(TreeNode root) {
+        if(root == null)
+            return;
+        
+        greaterRoot(root.right);
+        sum = sum + root.val;
+        root.val = sum;
+        greaterRoot(root.left);
+    }
+    public TreeNode convertBST(TreeNode root) {
+        Solution instance = new Solution();
+        instance.greaterRoot(root);
+        return root;
+    }
+}
