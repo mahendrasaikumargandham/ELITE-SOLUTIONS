8b2e3ee29a0e23d1849a7e486f6be5736e20e2a0
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2023-02-03 10:57:10 +0530

    Create LCA-of-2-Nodes-in-a-BST.java

---
+++ 03-02-2023/LCA-of-2-Nodes-in-a-BST.java
@@ -0,0 +1,51 @@
+import java.util.* ;
+import java.io.*; 
+/*
+
+    Following is the TreeNode class structure
+
+    class TreeNode<T> 
+    {
+       public:
+        T data;
+        TreeNode<T> left;
+        TreeNode<T> right;
+
+        TreeNode(T data) 
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    };
+
+*/
+
+public class Solution 
+{
+
+	public static TreeNode<Integer> LCAinaBST(TreeNode<Integer> root, TreeNode<Integer> p, TreeNode<Integer> q) 
+    {		
+        // Write your code here
+        if(root == null)
+            return null;
+
+        if(root == p || root == q) {
+            return root;
+        }
+
+        TreeNode<Integer> left = LCAinaBST(root.left, p, q);
+        TreeNode<Integer> right = LCAinaBST(root.right, p, q);
+
+        if(left != null && right != null)
+            return root;
+        
+        if(left != null)
+            return left;
+
+        if(right != null)
+            return right;
+        
+        return null;
+	}
+}
