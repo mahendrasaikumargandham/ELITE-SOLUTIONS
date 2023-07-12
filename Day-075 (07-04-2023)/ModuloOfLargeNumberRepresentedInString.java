80a1a9ea0333b1678ceab45358332202726ca352
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2023-07-12 13:20:14 +0530

    Created `ModuloOfLargeNumberRepresentedInString.java`

---
+++ Day-075 (07-04-2023)/ModuloOfLargeNumberRepresentedInString.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.nextLine();
+        System.out.println(Long.parseLong(str)%11);
+    }
+}
